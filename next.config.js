762e84e2367fbf804d94a6679df1bf46cfca6879
alfredjoe <66470948+alfredjoe@users.noreply.github.com>
2023-09-23 16:21:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,39 +2,3 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
-const Home = () => {
-  return (
-    <div className="root">
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert superheron related to your charactor</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>who areyou?</h2>
-          </div>
-        </div>
-        {/* Add this code here*/}
-        <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
-        </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
